2c4057e5d8a274d6681eed1969328f87c2f42cfe
haojue_wang <haojue.wang@oracle.com>
2019-01-10 03:35:31 +0000

    add filesystem.tf

--- examples/quick_start/datasource.tf
+++ examples/quick_start/datasource.tf
@@ -10,3 +10,13 @@ data "template_file" "ad_names" {
   count    = "${length(data.oci_identity_availability_domains.ad.availability_domains)}"
   template = "${lookup(data.oci_identity_availability_domains.ad.availability_domains[count.index], "name")}"
 }
+
+data "oci_core_private_ips" IPClusterFSMountTarget {
+  count     = 1
+  subnet_id = "${oci_core_subnet.slurmcontrol.id}" 
+
+  filter {
+    name   = "id"
+    values = ["${oci_file_storage_mount_target.TestClusterFSMountTarget.private_ip_ids}"]
+  }
+}
