e4b275aa9b75f5da1b7732a9d9a9024c8f493444
haojue_wang <haojue.wang@oracle.com>
2019-01-11 08:29:24 +0000

    output privateip of fs

--- examples/quick_start/outputs.tf
+++ examples/quick_start/outputs.tf
@@ -5,3 +5,8 @@ output "slurm_control_public_ip" {
 output "slurm_compute_private_ip" {
   value = "${module.slurm-cluster.compute_node_private_ips}"
 }
+
+output "slurm_fs_ip" {
+  value = "${data.oci_core_private_ips.IPClusterFSMountTarget.private_ips.0.ip_address}"
+}
+
--- modules/slurm-compute/scripts/setup.sh
+++ modules/slurm-compute/scripts/setup.sh
@@ -2,56 +2,6 @@
 
 set -e -x
 
-# Check URL if exists
-function validate_url() {
-    if wget -S --spider $1 2>&1 | grep 'HTTP/1.1 200 OK'
-    then
-        return 0
-    else
-        return 1
-    fi
-}
-
-# Add EPEL repo
-sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-
-# Installing required bits and bobs
-sudo yum install -y nfs-utils munge-devel munge-libs readline-devel perl-ExtUtils-MakeMaker openssl-devel pam-devel rpm-build gcc perl-DBI perl-Switch munge mariadb-devel
-
-# Oracle Object Storage Slurm RPM URL
-OOSURL="https://objectstorage.us-phoenix-1.oraclecloud.com/n/dxterraformdev/b/SlurmPackage/o/slurm-${slurm_version}-rpm.tar.gz"
-
-# Slurm Offical Download URL
-SCHEDMDURL="https://download.schedmd.com/slurm/slurm-${slurm_version}.tar.bz2"
-
-# Try Oracle Object Storage URL first to install Slurm
-if validate_url $OOSURL
-then
-    echo "Download Slurm RPM packages from Oracle Object Storage..."
-    wget $OOSURL
-    tar -xzvf slurm-${slurm_version}-rpm.tar.gz
-    sudo rpm -Uvh ~/RPMS/x86_64/*.rpm
-else
-    echo "Download Slurm RPM packages from SCHEDMD.COM..."
-    wget $SCHEDMDURL
-    # Building rpm packages
-    rpmbuild -ta slurm-${slurm_version}.tar.bz2
-    sudo rpm -Uvh ~/rpmbuild/RPMS/x86_64/*.rpm
-fi
-
-# Add user for slurm
-sudo useradd slurm
-sudo mkdir /var/log/slurm
-sudo chown slurm. /var/log/slurm
-sudo mkdir /var/spool/slurmd
-sudo chown slurm: /var/spool/slurmd
-sudo chmod 755 /var/spool/slurmd
-sudo touch /var/log/slurmd.log
-sudo chown slurm: /var/log/slurmd.log
-sudo cp /etc/slurm/cgroup.conf.example /etc/slurm/cgroup.conf
-
-#sudo mount.nfs  10.0.16.4:/shared /mnt/shared
-
 # Open the default ports that Slurm uses
 sudo firewall-cmd --permanent --zone=public --add-port=6817/udp
 sudo firewall-cmd --permanent --zone=public --add-port=6817/tcp
@@ -59,16 +9,3 @@ sudo firewall-cmd --permanent --zone=public --add-port=6818/tcp
 sudo firewall-cmd --permanent --zone=public --add-port=6819/tcp
 sudo firewall-cmd --permanent --zone=public --add-port=7321/tcp
 sudo firewall-cmd --reload
-
-
-# Get munge key from Slurm control node
-ssh -i ~/tmp.key -oStrictHostKeyChecking=no opc@${control_ip} "sudo cat /etc/munge/munge.key" > /home/opc/munge.key.tmp
-sudo mv munge.key.tmp /etc/munge/munge.key
-sudo chown -R munge: /etc/munge/ /var/log/munge/
-sudo chmod 0700 /etc/munge/ /var/log/munge/
-sudo chmod 400 /etc/munge/munge.key
-
-# Configure Munge auth daemon
-sudo systemctl start munge
-sudo systemctl enable munge
-sudo systemctl status munge
