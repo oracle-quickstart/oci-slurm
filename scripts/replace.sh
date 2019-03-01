d255a53f2ae0a1ec8e85ce4a4747786ff242343b
haojue_wang <haojue.wang@oracle.com>
2019-03-01 09:39:56 +0000

    replace ~/.oci/configbak with  ~/.oci/config

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
-echo "[DEFAULT]" >> ~/.oci/configbak
-echo "user=" >> ~/.oci/configbak
-echo "fingerprint=" >> ~/.oci/configbak
-echo "tenancy=" >> ~/.oci/configbak
-echo "region=" >> ~/.oci/configbak
+echo "[DEFAULT]" >> ~/.oci/config
+echo "user=" >> ~/.oci/config
+echo "fingerprint=" >> ~/.oci/config
+echo "tenancy=" >> ~/.oci/config
+echo "region=" >> ~/.oci/config
 
 replace=`cat terraform.tfvars | grep tenancy_ocid | awk -F= '{print $2}'`
-sed -i "s/tenancy=/tenancy=$replace/"  ~/.oci/configbak
+sed -i "s/tenancy=/tenancy=$replace/"  ~/.oci/config
 replace=`cat terraform.tfvars | grep user_ocid | awk -F= '{print $2}'`
-sed -i "s/user=/user=$replace/"  ~/.oci/configbak
+sed -i "s/user=/user=$replace/"  ~/.oci/config
 replace=`cat terraform.tfvars | grep fingerprint | awk -F= '{print $2}'`
-sed -i "s/fingerprint=/fingerprint=$replace/"  ~/.oci/configbak
+sed -i "s/fingerprint=/fingerprint=$replace/"  ~/.oci/config
 replace=`cat terraform.tfvars | grep region | awk -F= '{print $2}'`
-sed -i "s/region=/region=$replace/"  ~/.oci/configbak
+sed -i "s/region=/region=$replace/"  ~/.oci/config
 replace=`cat terraform.tfvars | grep private_key_path | awk -F= '{print $2}'`
-echo "key_file=$replace" >>  ~/.oci/configbak
+echo "key_file=$replace" >>  ~/.oci/config
