dc1bfadf7c94dea7a6726319528bfab9263e754b
haojue_wang <haojue.wang@oracle.com>
2019-03-04 06:56:31 +0000

    delete space and quote in ~/.oci/config

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
-echo "[DEFAULT]" >> ~/.oci/config
-echo "user=" >> ~/.oci/config
-echo "fingerprint=" >> ~/.oci/config
-echo "tenancy=" >> ~/.oci/config
-echo "region=" >> ~/.oci/config
+echo "[DEFAULT]" >> /home/opc/.oci/config
+echo "user=" >> /home/opc/.oci/config
+echo "fingerprint=" >> /home/opc/.oci/config
+echo "tenancy=" >> /home/opc/.oci/config
+echo "region=" >> /home/opc/.oci/config
 
 replace=`cat terraform.tfvars | grep tenancy_ocid | awk -F= '{print $2}'`
-sed -i "s/tenancy=/tenancy=$replace/"  ~/.oci/config
+sed -i "s/tenancy=/tenancy=$replace/"  /home/opc/.oci/config
 replace=`cat terraform.tfvars | grep user_ocid | awk -F= '{print $2}'`
-sed -i "s/user=/user=$replace/"  ~/.oci/config
+sed -i "s/user=/user=$replace/"  /home/opc/.oci/config
 replace=`cat terraform.tfvars | grep fingerprint | awk -F= '{print $2}'`
-sed -i "s/fingerprint=/fingerprint=$replace/"  ~/.oci/config
+sed -i "s/fingerprint=/fingerprint=$replace/"  /home/opc/.oci/config
 replace=`cat terraform.tfvars | grep region | awk -F= '{print $2}'`
-sed -i "s/region=/region=$replace/"  ~/.oci/config
+sed -i "s/region=/region=$replace/"  /home/opc/.oci/config
 replace=`cat terraform.tfvars | grep private_key_path | awk -F= '{print $2}'`
-echo "key_file=$replace" >>  ~/.oci/config
+echo "key_file=$replace" >>  /home/opc/.oci/config
